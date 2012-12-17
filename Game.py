c2c55b2b4010e8b548bbe911ac6080a2ff6c4140
pcsouza <souza.patrick@gmail.com>
2012-12-16 21:00:11 -0800

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,15 +1,19 @@
+#Game method object. Each game has its own betting method, so multiple game objects with unique betting behaviours
+#can be be initialized.
+
 import numpy as np
 import Hand, Betting
 
 class Game(object):
 	
 	def __init__(self, numplayers):
-		self.numplayers = numplayers
-		self.winnings = np.zeros((numplayers))
-		self.B = Betting.BettingMethods(numplayers)
+		self.numplayers = numplayers				#Number of players in the game
+		self.winnings = np.zeros((numplayers))			#Array storing the running total of all winnings
+		self.B = Betting.BettingMethods(numplayers)		#Initializes the betting method. Thus adaptive
+									#methods persist throughout the game.
 		self.BetFuncIndex = [self.B.HandBet, self.B.FlopBet, self.B.TurnBet, 
-			self.B.RiverBet]
-		self.verbose = False
+			self.B.RiverBet]				
+		self.verbose = False					#Trigger for verbose logging
 		
 		self.BetProfile = np.zeros((numplayers)) 	#Determines betting behaviour
 													#0 is simple bet. Bets 10 and always
@@ -18,16 +22,17 @@ class Game(object):
 													#linear relationship to winning chance
 													
 
-	
+#Main game method. Game is played by repeated calling of the PlayHand function.	
 	def PlayHand(self):
 		self.B.BetProfile = self.BetProfile
-		H = Hand.PlayHand(self.numplayers)
-		if self.verbose == True:
+		H = Hand.PlayHand(self.numplayers)	#Initialize a Hand object with all its methods
+		if self.verbose == True:		#Trigger verbose logging in other methods
 			H.verbose = True
 			self.B.verbose = True
 		else:
 			pass
-
+		
+		#Deal cards and betting actions.
 		H.DealHands()
 		self.B.HandBet(H)
 		
@@ -56,19 +61,7 @@ class Game(object):
 	
 
 
-# 	def DecideBets(stage):  	#stage indicates hand bet, flop bet, etc:
-# 		Bets = [0 for i in range(self.numplayers)]
-# 		StageBet = 0
-# 		for i in H.players:
-# 			C = self.BetFuncIndex[stage](i, Hands, Bets, StageBet)
-# 			if C == "fold":
-# 				
-# 			StageBet = max(Bets)
-# 		for i in range(self.numplayers):
-# 			if Bets[i] == "fold":
-# 				H.players.remove(i)
-# 		for i in H.players:
-# 			Bets[i] = self.BetFuncIndex[stage](i, Hands, Bets, StageBet)
+
 			
 			
 	
